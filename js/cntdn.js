c5c0cb0b4782e1d6706dfc6780f77e90933077e3
James Stanley <james@incoherency.co.uk>
2013-02-27 22:21:20 +0000

    Add (slow) numbers solver

--- js/cntdn.js
+++ js/cntdn.js
@@ -5,12 +5,7 @@
  * James Stanley 2013
  */
 
-function is_letter(letter) {
-    /* TODO: this */
-    return true;
-}
-
-function _recurse_solve(letters, node, used_letter, cb, answer) {
+function _recurse_solve_letters(letters, node, used_letter, cb, answer) {
     if (node[0])
         cb(answer);
 
@@ -22,20 +17,113 @@ function _recurse_solve(letters, node, used_letter, cb, answer) {
     for (var i = 0; i < letters.length; i++) {
         var c = letters.charAt(i);
 
-        if (used_letter[i] || !is_letter(c) || done[c])
+        if (used_letter[i] || done[c])
             continue;
 
         if (node[c]) {
             used_letter[i] = true;
             done[c] = true;
-            _recurse_solve(letters, node[c], used_letter, cb, answer+c);
+            _recurse_solve_letters(letters, node[c], used_letter, cb, answer+c);
             used_letter[i] = false;
         }
     }
 }
 
 function solve_letters(letters, cb) {
-    var used_letter = {};
+    _recurse_solve_letters(letters, dictionary, {}, cb, '');
+}
 
-    _recurse_solve(letters, dictionary, used_letter, cb, '');
+var OPS = {
+    "+": function(n1, n2) { if (n1 < 0 || n2 < 0) return false; return n1+n2; },
+    "-": function(n1, n2) { if (n2 >= n1) return false; return n1-n2; },
+    "*": function(n1, n2) { return n1*n2; },
+    "/": function(n1, n2) { if (n2 == 0 || n1%n2 != 0) return false; return n1/n2; },
+};
+
+var cache = {};
+
+function _recurse_solve_numbers(numbers, target, levels) {
+    if (levels == 0)
+        return false;
+
+    var key = numbers + ":" + target + ":" + (levels-1);
+
+    if (typeof cache[key] !== 'undefined')
+        return cache[key];
+
+    for (var i = 0; i < numbers.length-1; i++) {
+        var ni = numbers[i];
+
+        for (var j = i+1; j < numbers.length; j++) {
+            var nj = numbers[j];
+
+            /* try operations with both orders of arguments */
+            for (var x = 0; x < 2; x++) {
+
+                for (var o in OPS) {
+                    var r = OPS[o](ni, nj);
+                    if (r === false)
+                        continue;
+
+                    if (r == target)
+                        return [[ni, o, nj, r]];
+
+                    var xi = numbers[i];
+                    var xj = numbers[j];
+                    numbers[i] = r;
+                    numbers.splice(j, 1);
+
+                    var solution = _recurse_solve_numbers(numbers, target, levels-1);
+                    cache[key] = solution;
+                    if (solution !== false) {
+                        solution.unshift([ni, o, nj, r]);
+                        return solution;
+                    }
+
+                    numbers.splice(j, 0, xj);
+                    numbers[i] = xi;
+                }
+
+                /* now swap the args */
+                var nx = ni; ni = nj; nj = nx;
+            }
+        }
+    }
+
+    return false;
+}
+
+function solve_numbers(numbers, target) {
+    var diff = 0;
+
+    while (diff < 12) {
+        /* walk in a range around the target until we have an answer */
+        target += diff;
+        if (diff > 0)
+            diff = -diff;
+        else
+            diff = -diff + 1;
+
+        cache = {};
+
+        /* see if one of these numbers is the answer */
+        for (var i = 0; i < numbers.length; i++)
+            if (numbers[i] == target)
+                return [target, []];
+
+        /* attempt to solve for this target */
+        for (var i = 1; i <= numbers.length; i++) {
+            r = _recurse_solve_numbers(numbers, target, i);
+
+            if (r !== false) {
+                cache = {};
+                return [target, r];
+            }
+        }
+    }
+
+    cache = {};
+
+    /* no useful answer */
+    return false;
 }
--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,4 @@
-function solve() {
+function ui_solve_letters() {
     var letters = document.getElementById('letters').value;
 
     var result = [];
@@ -7,5 +7,46 @@ function solve() {
 
     result.sort(function(a, b) { return b.length - a.length; });
 
-    document.getElementById('answer').value = result.join("\n");
+    document.getElementById('letters-answer').value = result.join("\n");
+}
+
+function ui_solve_numbers() {
+    var numbers = [];
+
+    for (var i = 1; i <= 6; i++) {
+        var x = parseInt(document.getElementById('num'+i).value, 10);
+
+        if (!isNaN(x))
+            numbers.push(x);
+    }
+
+    var target = parseInt(document.getElementById('target').value, 10);
+    if (isNaN(target)) {
+        document.getElementById('numbers-answer').value = 'Invalid target';
+        return;
+    }
+
+    var r = solve_numbers(numbers, target);
+
+    if (r === false) {
+        document.getElementById('numbers-answer').value = 'No solution';
+    } else {
+        var result = r[0];
+        var explanation = '';
+
+        if (result != target)
+            explanation = '\n(off by ' + Math.abs(target - result) + ')';
+
+        if (r[1] != false) {
+            var text = r[1].map(
+                function(a) {
+                    return a[0] + " " + a[1] + " " + a[2] + " = " + a[3];
+                }
+            ).join("\n");
+        } else {
+            text = target + " = " + target;
+        }
+
+        document.getElementById('numbers-answer').value = text+explanation;
+    }
 }

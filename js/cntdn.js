505fc3f02310d44eb35dce20efc278b20d0ae886
James Stanley <james@incoherency.co.uk>
2013-03-10 17:00:33 +0000

    Use expression tree instead of stupid howto array, to allow much more sophisticated post-processing

--- js/cntdn.js
+++ js/cntdn.js
@@ -34,7 +34,6 @@ function solve_letters(letters, cb) {
 }
 
 var bestdiff;
-var bestops;
 var bestvalsums;
 
 var OPS = {
@@ -46,7 +45,7 @@ var OPS = {
     "?": function(n2, n1) { if (n2 == 0 || n1%n2 != 0) return false; return n1/n2; },
 };
 
-function _recurse_solve_numbers(numbers, searchedi, was_generated, target, levels, howto, valsums) {
+function _recurse_solve_numbers(numbers, searchedi, was_generated, target, levels, valsums) {
     levels--;
 
     for (var i = 0; i < numbers.length-1; i++) {
@@ -67,11 +66,9 @@ function _recurse_solve_numbers(numbers, searchedi, was_generated, target, level
                 continue;
 
             for (var o in OPS) {
-                var r = OPS[o](ni, nj);
+                var r = OPS[o](ni[0], nj[0]);
                 if (r === false)
                     continue;
-
-                howto.push([ni, o, nj, r]);
 
                 var op_cost = Math.abs(r);
                 while (op_cost % 10 == 0 && op_cost != 0)
@@ -79,21 +76,18 @@ function _recurse_solve_numbers(numbers, searchedi, was_generated, target, level
 
                 var newvalsums = valsums + op_cost;
 
-                if ((Math.abs(r - target) < Math.abs(bestresult - target))
-                        || (Math.abs(r - target) == Math.abs(bestresult - target) && newvalsums < bestvalsums)) {
-                    bestresult = r;
-                    bestops = howto.slice(0);
+                if ((Math.abs(r - target) < Math.abs(bestresult[0] - target))
+                        || (Math.abs(r - target) == Math.abs(bestresult[0] - target) && /*Math.floor(Math.random()*10) < 1)) {*/newvalsums < bestvalsums)) {
+                    bestresult = [r,ni,o,nj];
                     bestvalsums = newvalsums;
                 }
 
-                numbers[j] = r;
+                numbers[j] = [r, ni, o, nj];
                 var old_was_gen = was_generated[j];
                 was_generated[j] = true;
 
                 if (levels > 0)
-                    _recurse_solve_numbers(numbers, i+1, was_generated, target, levels, howto, newvalsums);
-
-                howto.pop();
+                    _recurse_solve_numbers(numbers, i+1, was_generated, target, levels, newvalsums);
 
                 was_generated[j] = old_was_gen;
                 numbers[j] = nj;
@@ -109,65 +103,111 @@ function tidyup_result(result) {
         "?": "/", "_": "-"
     };
 
-    var dontswap = {
-        "/": 1, "-": 1
+    var swappable = {
+        "*": true, "+": true
     };
 
-    for (var i = 0; i < result.length; i++) {
-        var subresult = result[i];
+    if (result.length != 4)
+        return result;
 
-        if (subresult[1] in mapping) {
-            subresult[1] = mapping[subresult[1]];
-            var j = subresult[0];
-            subresult[0] = subresult[2];
-            subresult[2] = j;
-        } else if (subresult[0] < subresult[2] && !(subresult[1] in dontswap)) {
-            var j = subresult[0];
-            subresult[0] = subresult[2];
-            subresult[2] = j;
-        }
-    }
+    result[1] = tidyup_result(result[1]);
+    result[3] = tidyup_result(result[3]);
 
-    /* simplify ordering of operations */
-    for (var i = 0; i < result.length - 1; i++) {
-        for (var j = i+1; j < result.length; j++) {
-            /* if result j-1 is not used in expression j, we can swap them */
-            if (result[j][0] != result[j-1][3]
-                    && result[j][2] != result[j-1][3]) {
-                var tmp = result[j-1];
-                result[j-1] = result[j];
-                result[j] = tmp;
-            }
-        }
+    if (result[2] in mapping) {
+        result[2] = mapping[result[2]];
+        var j = result[1];
+        result[1] = result[3];
+        result[3] = j;
+    } else if (result[1][0] < result[3][0] && swappable[result[2]]) {
+        var j = result[1];
+        result[1] = result[3];
+        result[3] = j;
     }
 
     return result;
 }
 
+function fullsize(array) {
+    if (array.constructor != Array)
+        return 0;
+
+    var l = 0;
+
+    for (var i = 0; i < array.length; i++)
+        l += fullsize(array[i]);
+
+    return l + array.length;
+}
+
+function serialise_result(result) {
+    if (result.length != 4)
+        return [];
+
+    var leftsteps = serialise_result(result[1]);
+    var rightsteps = serialise_result(result[3]);
+
+    /* do the shortest half second */
+    if (fullsize(leftsteps) < fullsize(rightsteps)) {
+        var t = leftsteps;
+        leftsteps = rightsteps;
+        rightsteps = t;
+    }
+
+    return leftsteps.concat(rightsteps, [[result[0], result[1][0], result[2], result[3][0]]]);
+}
+
+function stringify_result(serialised, target) {
+    var output = '';
+
+    serialised = serialised.slice(0);
+
+    var joinable = {
+        "*": true, "+": true
+    };
+
+    for (var i = 0; i < serialised.length; i++) {
+        var x = serialised[i];
+        var y = serialised[i+1];
+
+        if (i != serialised.length-1 && y[2] == x[2] && joinable[x[2]]) {
+            if (y[1] == x[0]) {
+                y[1] = x[1] + ' ' + x[2] + ' ' + x[3];
+            } else if(y[3] == x[0]) {
+                y[3] = x[1] + ' ' + x[2] + ' ' + x[3];
+            }
+        } else {
+            output += x[1] + ' ' + x[2] + ' ' + x[3] + ' = ' + x[0] + '\n';
+        }
+    }
+
+    var result = serialised[serialised.length-1][0];
+    if (result != target)
+        output += '(off by ' + (Math.abs(result - target)) + ')\n';
+
+    return output;
+}
+
 function solve_numbers(numbers, target) {
     numbers.sort();
-    bestresult = numbers[0];
-    bestops = [];
+    bestresult = [numbers[0], numbers[0]];
+
+    numbers = numbers.map(function(x) { return [x, false] });
 
     /* see if one of these numbers is the answer */
     for (var i = 1; i < numbers.length; i++) {
-        if (Math.abs(numbers[i] - target) < Math.abs(bestresult - target)) {
-            bestresult = numbers[i];
-            bestops = [];
+        if (Math.abs(numbers[i] - target) < Math.abs(bestresult[0] - target)) {
+            bestresult = [numbers[i], numbers[i]];
             bestvalsums = numbers[i];
         }
     }
-    if (bestresult == target)
-        return [bestresult, bestops];
+    if (bestresult[0] == target)
+        return target + " = " + target;
 
     var was_generated = [];
     for (var i = 0; i < numbers.length; i++)
         was_generated.push(false);
 
     /* attempt to solve with dfs */
-    _recurse_solve_numbers(numbers, 0, was_generated, target, numbers.length, [], 0);
-    bestops = tidyup_result(bestops);
-
-    /* return best answer */
-    return [bestresult, bestops];
+    _recurse_solve_numbers(numbers, 0, was_generated, target, numbers.length, 0);
+    return stringify_result(serialise_result(tidyup_result(bestresult)), target);
 }
--- js/ui.js
+++ js/ui.js
@@ -31,27 +31,5 @@ function ui_solve_numbers() {
         return;
     }
 
-    var r = solve_numbers(numbers, target);
-
-    if (r === false) {
-        document.getElementById('numbers-answer').value = 'No solution';
-    } else {
-        var result = r[0];
-        var explanation = '';
-
-        if (result != target)
-            explanation = '\n(off by ' + Math.abs(target - result) + ')';
-
-        if (r[1] != false) {
-            var text = r[1].map(
-                function(a) {
-                    return a[0] + " " + a[1] + " " + a[2] + " = " + a[3];
-                }
-            ).join("\n");
-        } else {
-            text = result + " = " + result;
-        }
-
-        document.getElementById('numbers-answer').value = text+explanation;
-    }
+    document.getElementById('numbers-answer').value = solve_numbers(numbers, target);
 }
